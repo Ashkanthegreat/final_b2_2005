909f65a131611c99c2d63da0053a9d97a0b85430
Ashkan Abbasi <airashkan@yahoo.com>
2020-08-03 12:27:20 -0600

    Make passenger model methods tests

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Passenger, type: :model do
     it {should have_many :flights}
     it {should have_many(:flights).through(:flight_passengers)}
   end
+
+  describe 'methods' do
+    before :each do
+      @jim = Passenger.create(name: "Jim", age: 40)
+      @barb = Passenger.create(name: "Barbara", age: 35)
+      @jim_jr = Passenger.create(name: "Jim Jr", age: 15)
+    end
+
+    it "#minors" do
+      expect(Passenger.minors).to eq(1)
+    end
+
+    it "#adults" do
+      expect(Passenger.adults).to eq(2)
+    end
+  end
 end
