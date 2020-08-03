0c83a7bc571350d7c21c25adce7616508e88914a
Ashkan Abbasi <airashkan@yahoo.com>
2020-08-03 10:24:32 -0600

    Pass model relationship tests

--- app/models/flight_passenger.rb
+++ app/models/flight_passenger.rb
@@ -1,4 +1,4 @@
 class FlightPassenger < ApplicationRecord
-  belong_to :flight
+  belongs_to :flight
   belongs_to :passenger
 end
--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -5,12 +5,28 @@ RSpec.describe "Flights Show Page" do
     it "can see a flights info, the airline it belongs to, and all of the passengers on the flight" do
       frontier = Airline.create(name: "Frontier")
 
-      reno = frontier.flights.create(number: "1550", date: "08/03/20", time: "1800", depart_city: "Denver", arrive_city: "Reno")
+      reno = frontier.flights.create(number: "1550", date: "08/03/20", time: "1800", depar_city: "Denver", arrive_city: "Reno")
 
       jim = Passenger.create(name: "Jim", age: 40)
       jim_jr = Passenger.create(name: "Jim Jr", age: 15)
 
-      
+      FlightPassenger.create!(flight: reno, passenger: jim)
+      FlightPassenger.create!(flight: reno, passenger: jim_jr)
+
+      visit "/flights/#{reno.id}"
+
+      expect(page).to have_content(reno.number)
+      expect(page).to have_content(reno.date)
+      expect(page).to have_content(reno.time)
+      expect(page).to have_content(reno.depar_city)
+      expect(page).to have_content(reno.arrive_city)
+      expect(page).to have_content("Frontier")
+
+
+
+
+
+
     end
 
   end
@@ -28,5 +44,3 @@ end
 #   - arrival city
 # And I see the name of the airline this flight belongs
 # And I see the names of all of the passengers on this flight
-# ```
-​
