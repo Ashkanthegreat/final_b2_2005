bf06394ffe7409a6c3642b87c0f494a56aea2668
Ashkan Abbasi <airashkan@yahoo.com>
2020-08-03 12:32:34 -0600

    Fix error

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,13 +1,11 @@
 class FlightPassengersController < ApplicationController
   def create
     flight = Flight.where(number: params[:number]).first
-    fp = FlightPassenger.where(flight: flight, passenger_id: params[:passenger_id]).first
+    flight_passenger = FlightPassenger.where(flight: flight, passenger_id: params[:passenger_id]).first
     if flight.nil?
       flash[:error] = "The flight number you entered does not exist"
-    elsif fp.nil? && Flight.exists?(flight.id)
+    else flight_passenger.nil? && Flight.exists?(flight.id)
       FlightPassenger.create(flight: flight, passenger_id: params[:passenger_id])
-    elsif FlightPassenger.exists?(fp.id)
-      flash[:error] = "Passenger is already on this flight"
     end
     redirect_to "/passengers/#{params[:passenger_id]}"
   end
